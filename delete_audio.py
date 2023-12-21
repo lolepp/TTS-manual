129d924091cedf75dadfdb9d6502230bc754eb25
lolepp <rauch-philipp@gmx.de>
2023-12-21 11:35:30 +0100

    TTS fixed and improved script

--- delete_audio.py
+++ delete_audio.py
@@ -1,14 +1,15 @@
 import os
 
-# Directory where WAV files are located
-lib = "audio/"
+# Directories where audio files are located
+libs = ["audio/wav/en/", "audio/wav/de/", "audio/mp3/en/", "audio/mp3/de/"]
 
 # List all files in the directory
-file_list = os.listdir(lib)
+for lib in libs:
+    file_list = os.listdir(lib)
 
-# Iterate through the files and delete WAV files
-for file in file_list:
-    if file.endswith(".wav"):
-        file_path = os.path.join(lib, file)
-        os.remove(file_path)
-        print(f"Deleted: {file_path}")
+    # Iterate through the files and delete files
+    for file in file_list:
+        if file.endswith(".wav") or file.endswith(".mp3"):
+            file_path = os.path.join(lib, file)
+            os.remove(file_path)
+            print(f"Deleted: {file_path}")
--- tts.py
+++ tts.py
@@ -1,14 +1,34 @@
 from gtts import gTTS
+from pydub import AudioSegment
+import os
 
-# Your file from the lib directory here, which shall be spoken
-text_file = "quandale_dingle"
+# The lib directories, which shall be TTSed
+lib_en = "text/en/"
+lib_de = "text/de/"
 
-lib = "./lib/"
-input_path = lib + text_file + ".txt"
-output_path_wav = "audio/" + text_file + ".wav"
+files_en = [os.path.splitext(f)[0] for f in os.listdir(lib_en) if os.path.isfile(os.path.join(lib_en, f))]
+files_de = [os.path.splitext(f)[0] for f in os.listdir(lib_de) if os.path.isfile(os.path.join(lib_de, f))]
 
-with open(input_path, "r", encoding="utf-8") as file:
-    text = file.read()
+def tts(input, output, langs):
+    with open(input, "r", encoding="utf-8") as file:
+        text = file.read()
+    tts = gTTS(text, lang=langs)
+    tts.save(output + ".wav")
 
-tts = gTTS(text)
-tts.save(output_path_wav)
+def convertWavToMp3(path, to):
+    sound = AudioSegment.from_wav(path + ".wav")
+    sound.export(to + ".mp3", format="mp3", bitrate="192k")
+
+def main():
+    for file in files_en:
+        tts(lib_en + file + ".txt", "audio/wav/en/" + file, "en")
+    for file in files_de:
+        tts(lib_de + file + ".txt", "audio/wav/de/" + file, "de")
+    # Conversion does not work on me
+    # for file in files_en:
+    #     convertWavToMp3("audio/wav/en/" + file, "audio/mp3/en/" + file)
+    # for file in files_de:
+    #     convertWavToMp3("audio/wav/de/" + file, "audio/mp3/de/" + file)
+
+if __name__ == "__main__":
+    main()
